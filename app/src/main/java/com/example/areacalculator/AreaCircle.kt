f6e8b9d4ff9ea61ab62f3d4fe2bd8c1fc308687b
Omar Juma <jumao2669@gmail.com>
2023-02-12 22:29:00 +0300

    area of circle is working

--- app/src/main/java/com/example/areacalculator/AreaCircle.kt
+++ app/src/main/java/com/example/areacalculator/AreaCircle.kt
@@ -2,10 +2,24 @@ package com.example.areacalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class AreaCircle : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_area_circle)
+//        val pi:Double=3.142
+
+        var btnAreaCircle=findViewById<Button>(R.id.btnAreaCircle)
+        var edTxtRadius=findViewById<EditText>(R.id.edTxtRadius)
+        var txtAnswer=findViewById<TextView>(R.id.txtAnswer)
+
+        btnAreaCircle.setOnClickListener() {
+            var area=3.142*edTxtRadius.text.toString().toDouble()*edTxtRadius.text.toString().toDouble()
+            txtAnswer.text = area.toString()
+
+       }
     }
 }
---
+++ app/src/main/java/com/example/areacalculator/AreaOfSphere.kt
@@ -0,0 +1,11 @@
+package com.example.areacalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AreaOfSphere : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_area_of_sphere)
+    }
+}
